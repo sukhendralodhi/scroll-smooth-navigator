75d986271c3b0cce7b10a1cc3a1967b8ccf3fb43
Sukhendra Lodhi <mpct5jan1998@gmail.com>
2023-04-19 16:16:39 +0530

    sticky navbar modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll(".links");
 
-for (link of links) {
-  link.addEventListener("click", (e) => {
+for (linkEle of links) {
+  linkEle.addEventListener("click", (e) => {
     e.preventDefault();
     const href = e.target.getAttribute("href");
     document.querySelector(href).scrollIntoView({
@@ -9,3 +9,23 @@ for (link of links) {
     });
   });
 }
+
+// active menu switcher
+const navMenu = document.querySelector("nav");
+const activeMenu = (e) => {
+  const navList = e.target;
+  if (navList.classList.contains("links")) {
+    navMenu
+      .querySelector(".text-orange-600")
+      .classList.remove("text-orange-600");
+    navList.classList.add("text-orange-600");
+  }
+};
+
+navMenu.addEventListener("click", activeMenu);
+
+const stickyNav = () => {
+  const header = document.getElementById("header");
+  header.classList.toggle("bg-gray-400", window.scrollY > 0);
+};
+window.addEventListener("scroll", stickyNav);
